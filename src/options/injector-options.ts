369592cbd841380e470a3c092e9126ddb834671b
pashaman <plavel@gmail.com>
2026-06-16 21:10:22 +0700

    feat(acess-control): updated injector options

--- src/options/injector-options.ts
+++ src/options/injector-options.ts
@@ -1,10 +1,11 @@
 import { AladoServer } from '@alado';
-import { FilePropertyDefinition, PropertyDefinition, RequestAuthentication } from '@dto';
+import { AccessControlConfig, FilePropertyDefinition, PropertyDefinition, RequestAuthentication } from '@dto';
 
 export type InjectorOptions = {
   appMapping: Record<string, AladoServer>;
   shadowAppMapping: Record<string, any>;
   authMapping: Record<string, RequestAuthentication>;
+  accessControlMapping: Record<string, AccessControlConfig<any>[]>;
   requestMapping: Record<string, Record<string, Record<string, PropertyDefinition | FilePropertyDefinition>>>;
   responseMapping: Record<string, any>;
   controllers: Record<string, Object>;
