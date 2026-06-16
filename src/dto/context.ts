428291ae2d6d5c8c88240818ccd817b0f45f1ec7
pashaman <plavel@gmail.com>
2026-06-16 21:08:55 +0700

    feat(acess-control): updated context dto

--- src/dto/context.ts
+++ src/dto/context.ts
@@ -2,10 +2,12 @@ import { ContextOptions } from '@options';
 import { RequestAuthentication } from './request-authentication';
 import { Response } from './response';
 import { ContextRequest } from './request';
+import { AccessControlConfig } from './access-control-config';
 
 export type Context<T> = {
   title: string;
   auth?: RequestAuthentication;
+  accessControl?: AccessControlConfig<any>[];
   options: ContextOptions;
   request: ContextRequest;
   response: Response<Record<keyof T, any>>;
