049bac52d941c3c771f7a90f3cbdeca6dbc7db9e
pashaman <plavel@gmail.com>
2026-06-16 21:13:36 +0700

    feat(acess-control): added access-control-checker helper

---
+++ src/helpers/parser/access-control-checker.ts
@@ -0,0 +1,33 @@
+import { AladoServerError, Context } from '@dto';
+import { getProperty } from '../accessor';
+
+export async function accessControlChecker(context: Context<any>, request: any): Promise<AladoServerError | void> {
+  const { accessControl } = context;
+  let success: boolean = true;
+  for (const item of accessControl) {
+    const { inputProperty, transformInputProperty, statusCode, message, compareWithProperty, expectedValue } = item;
+
+    let providedValue: unknown = getProperty(request, inputProperty);
+
+    if (typeof transformInputProperty === 'function') {
+      providedValue = await transformInputProperty(providedValue);
+    }
+
+    if (expectedValue !== undefined) {
+      success = expectedValue === providedValue;
+    }
+
+    if (!success) {
+      return { statusCode, message };
+    }
+
+    if (compareWithProperty) {
+      const compareWithVale = getProperty(request, compareWithProperty);
+      success = compareWithVale === providedValue;
+    }
+
+    if (!success) {
+      return { statusCode, message };
+    }
+  }
+}
--- src/helpers/parser/index.ts
+++ src/helpers/parser/index.ts
@@ -1,3 +1,4 @@
+export * from './access-control-checker';
 export * from './authenticate';
 export * from './body-parser';
 export * from './query-parser';
